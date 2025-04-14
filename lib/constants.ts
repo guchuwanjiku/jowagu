a60d4071557b2ef62a6838f04c1911dc1ad16d21
guchuwanjiku <peterjoseph084@gmail.com>
2025-04-14 22:49:25 +0300

    2-create-web-layout

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Jowagu'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Next js app'
+
+// export const USER_ROLES = ['Admin', 'User']
+// export const COLORS = ['Gold', 'Green', 'Red']
+// export const THEMES = ['Light', 'Dark', 'System']
