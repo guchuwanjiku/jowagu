289eee91859afdc0b91bc708b801f41763486f49
guchuwanjiku <peterjoseph084@gmail.com>
2025-05-05 09:13:11 +0300

    24-theme

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default async function LoadingPage() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <div className='p-6 rounded-lg w-1/3 shadow-md text-center'>
+        Loading...
+      </div>
+    </div>
+  )
+}
